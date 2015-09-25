1a78189819b66373fe696f5cad7fd39b9889cd6c
Andrew Morris <voltrevo@gmail.com>
2015-09-25 17:21:15 +1000

    directory creation

--- test/index.js
+++ test/index.js
@@ -2,15 +2,53 @@
 
 /* global describe it */
 
+// core modules
 var assert = require('assert');
+var path = require('path');
+
+// community modules
+var tmp = require('tmp');
+
+// local modules
 var abstractFs = require('../lib');
 
+// transformed modules
+var fs = require('thenify-all')(
+  require('fs'),
+  {},
+  ['lstat']
+);
+
+var tmpDirPath = tmp.dirSync({
+  unsafeCleanup: true
+}).name;
+
+var nextDir = (function() {
+  var counter = 0;
+
+  return function() {
+    return path.join(tmpDirPath, 'testDir' + counter++);
+  };
+})();
+
 describe('abstract-fs', function() {
+  it('can create a directory', function() {
+    var dirPath = nextDir();
+
+    return abstractFs.System(dirPath).create().then(function() {
+      return fs.lstat(dirPath);
+    }).then(function(stats) {
+      assert(stats.isDirectory());
+    });
+  });
+
+  /*
   it('can write a file to a directory', function() {
-    return abstractFs.System('/tmp').Dir(uuid()).create().then(function(dir) {
-      return dir.File('foo').write(Buffer('bar'));
+    return abstractFs.System(nextDir()).then(function(dir) {
+      return dir.File('foo').write(new Buffer('bar'));
     }).then(function() {
       // TODO: assert foo created with content 'bar'
     });
   });
+  */
 });
