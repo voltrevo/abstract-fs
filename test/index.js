4e3ebd6ed3defeb32e4af7bb9d1f1befa06438e5
Andrew Morris <voltrevo@gmail.com>
2015-09-25 17:29:35 +1000

    Use async tmp.dir

--- test/index.js
+++ test/index.js
@@ -1,40 +1,60 @@
 'use strict';
 
-/* global describe it */
+/* global describe it beforeEach afterEach */
 
 // core modules
 var assert = require('assert');
 var path = require('path');
 
 // community modules
-var tmp = require('tmp');
+var thenifyAll = require('thenify-all');
 
 // local modules
 var abstractFs = require('../lib');
 
 // transformed modules
-var fs = require('thenify-all')(
+var fs = thenifyAll(
   require('fs'),
   {},
   ['lstat']
 );
 
-var tmpDirPath = tmp.dirSync({
+var tmp = thenifyAll(
+  require('tmp'),
+  {},
+  ['dir']
+);
+
+var tmpDirPath = tmp.dir({
   unsafeCleanup: true
-}).name;
+}).then(function(res) {
+  return res[0];
+});
 
 var nextDir = (function() {
   var counter = 0;
 
   return function() {
-    return path.join(tmpDirPath, 'testDir' + counter++);
+    return tmpDirPath.then(function(tdp) {
+      return path.join(tdp, 'testDir' + counter++);
+    });
   };
 })();
 
 describe('abstract-fs', function() {
-  it('can create a directory', function() {
-    var dirPath = nextDir();
+  var dirPath = undefined;
 
+  beforeEach(function() {
+    return nextDir().then(function(nd) {
+      dirPath = nd;
+    });
+  });
+
+  afterEach(function() {
+    dirPath = undefined;
+  });
+
+  it('can create a directory', function() {
     return abstractFs.System(dirPath).create().then(function() {
       return fs.lstat(dirPath);
     }).then(function(stats) {
